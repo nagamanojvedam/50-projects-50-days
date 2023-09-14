c6fe3e45bd6ac81072bd6e5c13e6ca21badc60ac
nagamanojvedam <nagamanojvedam@gmail.com>
2023-09-14 11:33:36 +0530

    completed expanding cards

---
+++ expanding-cards/script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const panels = document.querySelectorAll('.panel');
+
+const removeActive = () => {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+};
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActive();
+    panel.classList.add('active');
+  });
+});
