2c25c28f7f79ebbb8a03c78fdb2a87eada0e1ea6
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-22 23:42:45 +0530

    added my code

---
+++ hidden-search/script.js
@@ -0,0 +1,7 @@
+const searchBtn = document.getElementById('search-btn');
+const inputBox = document.querySelector('.input-text-box');
+const containerEl = document.querySelector('.container');
+
+searchBtn.addEventListener('click', () => {
+  inputBox.classList.toggle('hidden');
+});
