1ae874f1684e4c6dc77306eb39eb2e5b026bc571
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-23 13:34:02 +0530

    add progress bar update functionality

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -8,25 +8,29 @@ const progressBarEl = document.querySelector('.progress-bar');
 let currentValue = 0;
 let compValue = 1;
 
-next.addEventListener('click', () => {
+next.addEventListener('click', (evt) => {
   if (currentValue >= 5) {
     currentValue = 5;
   } else {
     currentValue++;
   }
-  console.log(currentValue);
-
-  progressEl[currentValue - 1].classList.add('active');
-  progressBarEl.style.width = `${(currentValue - 1) * 100}px`;
+  updateProgressBar(evt.target, currentValue);
 });
 
-prev.addEventListener('click', () => {
+prev.addEventListener('click', (evt) => {
   if (currentValue <= 0) {
     currentValue = 0;
   } else {
     currentValue--;
   }
-  console.log(currentValue);
-  progressEl[currentValue].classList.remove('active');
-  progressBarEl.style.width = `${(currentValue - 1) * 100}px`;
+  updateProgressBar(evt.target, currentValue);
 });
+
+function updateProgressBar(target, currentValue) {
+  if (target.id === 'next')
+    progressEl[currentValue - 1].classList.add('active');
+
+  if (target.id === 'prev') progressEl[currentValue].classList.remove('active');
+
+  progressBarEl.style.width = `${(currentValue - 1) * 100}px`;
+}
