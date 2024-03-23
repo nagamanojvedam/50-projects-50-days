a32e236120edb6441360e292ab03309201a70c22
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-23 18:50:42 +0530

    add functionality

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const menuBtn = document.getElementById('menu');
+const closeBtn = document.getElementById('close');
+const navigationEl = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click', () => {
+  navigationEl.classList.remove('rotate-down');
+  navigationEl.classList.add('rotate-up');
+});
+
+closeBtn.addEventListener('click', () => {
+  navigationEl.classList.remove('rotate-up');
+  navigationEl.classList.add('rotate-down');
+});
