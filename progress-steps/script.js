58f799ba2c2e0594ba14463ea1236e333ed250f8
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-23 13:09:08 +0530

    add prev and next btn functionality

--- progress-steps/script.js
+++ progress-steps/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+const progressEl = document.querySelectorAll('.progress-item');
+const progressBarEl = document.querySelector('.progress-bar');
+let currentValue = 0;
+let compValue = 1;
+
+next.addEventListener('click', () => {
+  if (currentValue >= 5) {
+    currentValue = 5;
+  } else {
+    currentValue++;
+  }
+  console.log(currentValue);
+
+  progressEl[currentValue - 1].classList.add('active');
+  progressBarEl.style.width = `${(currentValue - 1) * 100}px`;
+});
+
+prev.addEventListener('click', () => {
+  if (currentValue <= 0) {
+    currentValue = 0;
+  } else {
+    currentValue--;
+  }
+  console.log(currentValue);
+  progressEl[currentValue].classList.remove('active');
+  progressBarEl.style.width = `${(currentValue - 1) * 100}px`;
+});
