11fcd2f6fb6df584e7d2e909c3bc7901ab3425b8
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-24 22:27:51 +0530

    add nav menu style and transition

--- rotating-navigation/script.js
+++ rotating-navigation/script.js
@@ -1,20 +1,23 @@
 'use strict';
 
-const menuBtn = document.getElementById('menu');
+const barsBtn = document.getElementById('bars');
 const closeBtn = document.getElementById('close');
 const navigationEl = document.querySelector('.navigation');
 const containerEl = document.querySelector('.container');
+const menuEl = document.querySelector('.menu');
 
 console.log(containerEl);
 
-menuBtn.addEventListener('click', () => {
+barsBtn.addEventListener('click', () => {
   navigationEl.classList.remove('rotate-menu-down');
   navigationEl.classList.add('rotate-menu-up');
   containerEl.classList.add('rotate-container');
+  menuEl.classList.add('show-menu');
 });
 
 closeBtn.addEventListener('click', () => {
   navigationEl.classList.remove('rotate-menu-up');
   navigationEl.classList.add('rotate-menu-down');
   containerEl.classList.remove('rotate-container');
+  menuEl.classList.remove('show-menu');
 });
