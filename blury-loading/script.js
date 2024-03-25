99858580cbf51fc0713d2e3949d47338af0b47cc
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-25 21:33:24 +0530

    add blur functionality

--- blury-loading/script.js
+++ blury-loading/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Tesing JS');
+const containerEL = document.querySelector('.container');
+const blurContainerEl = document.querySelector('.blur-container');
+
+let load = 0;
+let interval = setInterval(blurryEffect, 30);
+
+function blurryEffect() {
+  load++;
+
+  if (load > 99) clearInterval(interval);
+
+  console.log(load);
+
+  blurContainerEl.textContent = `${load}% Loading`;
+  blurContainerEl.style.opacity = mapNumRange(load, 0, 100, 1, 0);
+  containerEL.style.filter = `blur(${mapNumRange(load, 0, 100, 50, 0)}px)`;
+}
+
+const mapNumRange = (num, inMin, inMax, outMin, outMax) =>
+  ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
