a614ce4fe7f6194fcdf1660652cf23fe96048107
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-03-30 15:59:35 +0530

    add date counter v2

--- date-counter/AppV1.js
+++ date-counter/AppV1.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './App.css';
+import './AppV1.css';
 
 const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat'];
 
---
+++ date-counter/AppV2.js
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import "./AppV2.css";
+
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+
+const month = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const offSet = step * count;
+  const date = new Date();
+  date.setDate(date.getDate() + offSet);
+
+  function handleStep(evnt) {
+    setStep(evnt.target.value);
+  }
+
+  function handleDecreaseCount() {
+    setCount((curr) => curr - 1);
+  }
+  function handleIncreaseCount() {
+    setCount((curr) => curr + 1);
+  }
+  return (
+    <div className="app">
+      <div className="step">
+        <input
+          type="range"
+          min={1}
+          max={10}
+          defaultValue={1}
+          onChange={handleStep}
+        />
+        <p>{step}</p>
+      </div>
+      <div className="item">
+        <button onClick={handleDecreaseCount}>-</button>
+        <input type="text" value={count} onChange={() => count} />
+        <button onClick={handleIncreaseCount}>+</button>
+      </div>
+      <p className="para">
+        {`${
+          offSet
+            ? `${Math.abs(offSet)} days ${
+                count < 0 ? `ago from today was` : `from today is`
+              }`
+            : `Today is`
+        } ${weekday[date.getDay()]} ${
+          month[date.getMonth()]
+        } ${date.getDate()} ${date.getFullYear()}`}
+      </p>
+    </div>
+  );
+}
