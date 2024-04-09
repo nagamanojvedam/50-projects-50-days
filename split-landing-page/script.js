db174a818cbc4efc1fcdff934b9fd5fe3c963a61
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-04-09 15:57:23 +0530

    add mouse hover transition

--- split-landing-page/script.js
+++ split-landing-page/script.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('This is new split landing page.');
+const leftEl = document.querySelector('.left');
+const rightEl = document.querySelector('.right');
+const containerEl = document.querySelector('.container');
+
+leftEl.addEventListener('mouseover', () =>
+  containerEl.classList.add('hover-left')
+);
+leftEl.addEventListener('mouseleave', () =>
+  containerEl.classList.remove('hover-left')
+);
+
+rightEl.addEventListener('mouseover', () =>
+  containerEl.classList.add('hover-right')
+);
+rightEl.addEventListener('mouseleave', () =>
+  containerEl.classList.remove('hover-right')
+);
