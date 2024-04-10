828e3e77c56545698babd339d7786f3b2008bb67
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-04-10 12:44:05 +0530

    added my code to play audio

--- sound-board/script.js
+++ sound-board/script.js
@@ -1,3 +1,12 @@
 'use strict';
 
 console.log(`This is a sound board`);
+
+const audioBoxes = document.querySelectorAll('.audio-item');
+
+audioBoxes.forEach((audioBox) => {
+  audioBox.addEventListener('click', () => {
+    const audio = new Audio(`./sounds/${audioBox.innerText.toLowerCase()}.mp3`);
+    audio.play();
+  });
+});
