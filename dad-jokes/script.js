2094ae82e7843142f6fe8c10be2ff53db2708312
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-04-12 10:11:46 +0530

    added fetching joke api

--- dad-jokes/script.js
+++ dad-jokes/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
-console.log('Dad Jokes');
+const BASE_URL = `https://icanhazdadjoke.com/`;
+const config = {
+  headers: {
+    Accept: 'application/json',
+  },
+};
+
+const getJokeBtn = document.querySelector('.btn');
+const jokeEl = document.querySelector('#joke');
+
+getJoke();
+
+getJokeBtn.addEventListener('click', getJoke);
+
+async function getJoke() {
+  const response = await fetch(BASE_URL, config);
+  const data = await response.json();
+
+  jokeEl.innerHTML = data.joke;
+}
