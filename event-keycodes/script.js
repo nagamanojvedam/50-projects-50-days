3737db3f30685deb32fb553458ab1071491439a3
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-04-12 11:55:14 +0530

    added keypress functionality

--- event-keycodes/script.js
+++ event-keycodes/script.js
@@ -1,3 +1,19 @@
 'use strict';
 
-console.log('Event Keycodes');
+const firstEl = document.querySelector('.first');
+const secondEl = document.querySelector('.second');
+
+const eventKeyEl = document.querySelector('.event-key');
+const eventKeyCodeEl = document.querySelector('.event-key-code');
+const eventCodeEl = document.querySelector('.event-code');
+
+window.addEventListener('keydown', (evnt) => {
+  if (secondEl.classList.contains('hidden')) {
+    firstEl.classList.add('hidden');
+    secondEl.classList.remove('hidden');
+  }
+
+  eventKeyEl.innerText = evnt.key;
+  eventKeyCodeEl.innerText = evnt.key.charCodeAt(0);
+  eventCodeEl.innerText = evnt.code;
+});
