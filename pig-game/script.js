a71e1971ad81ac7b88262c8611dc6394b36f4a75
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-09-09 11:50:02 +0530

    revised game script

--- pig-game/script.js
+++ pig-game/script.js
@@ -6,20 +6,15 @@ const holdBtn = document.querySelector('.btn--hold');
 
 const diceImgEl = document.querySelector('.dice');
 
-const score0El = document.querySelector('#score--0');
-const score1El = document.querySelector('#score--1');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
-
-const player0BoardEl = document.querySelector('.player--0');
-const player1BoardEl = document.querySelector('.player--1');
+const scoresEl = document.querySelectorAll('.score');
+const currentScoresEl = document.querySelectorAll('.current-score');
+const playersBoardEl = document.querySelectorAll('.player');
 
 let player = [0, 0];
 let currentPlayer = 0;
 let currentScore = 0;
 
-score0El.textContent = `0`;
-score1El.textContent = `0`;
+resetGame();
 
 newGameBtn.addEventListener('click', () => {
   resetGame();
@@ -29,80 +24,59 @@ rollBtn.addEventListener('click', () => {
   const rolledDice = Math.trunc(Math.random() * 6) + 1;
   diceImgEl.src = `dice-${rolledDice}.png`;
 
-  if (rolledDice === 1) {
-    // set current score to 0
-    currentScore = 0;
-    currentPlayer === 0
-      ? (current0El.textContent = currentScore)
-      : (current1El.textContent = currentScore);
-    // change player
-    changePlayer();
-  } else {
-    currentScore += rolledDice;
-    diceImgEl.src = `dice-${rolledDice}.png`;
-    currentPlayer === 0
-      ? (current0El.textContent = currentScore)
-      : (current1El.textContent = currentScore);
-  }
+  currentScore = rolledDice === 1 ? changePlayer() : currentScore + rolledDice;
+
+  currentScoresEl[currentPlayer].textContent = currentScore;
 });
 
 holdBtn.addEventListener('click', () => {
-  // update player score
   player[currentPlayer] += currentScore;
-  // check winner
   checkWinner();
-  currentPlayer === 0
-    ? (score0El.textContent = player[currentPlayer])
-    : (score1El.textContent = player[currentPlayer]);
-  // set current score to 0
-  currentScore = 0;
-  currentPlayer === 0
-    ? (current0El.textContent = currentScore)
-    : (current1El.textContent = currentScore);
-  // change player
-  // change the active class to respective player
+
+  scoresEl[currentPlayer].textContent = player[currentPlayer];
   changePlayer();
 });
 
 function changePlayer() {
-  // toggle current player
+  currentScore = 0;
+  currentScoresEl[currentPlayer].textContent = currentScore;
+  playersBoardEl[currentPlayer].classList.toggle('player--active');
   currentPlayer = currentPlayer === 0 ? 1 : 0;
-  player0BoardEl.classList.toggle('player--active');
-  player1BoardEl.classList.toggle('player--active');
+  playersBoardEl[currentPlayer].classList.toggle('player--active');
+
+  return currentScore;
 }
 
 function resetGame() {
-  // set current player to 0
   currentPlayer = 0;
-  // empty player array
   player = [0, 0];
-  // set current score to 0
   currentScore = 0;
-  // set text content of scores to 0
-  score0El.textContent = `0`;
-  score1El.textContent = `0`;
-  // set active class to player 0
-  player0BoardEl.classList.remove('active');
-  player1BoardEl.classList.remove('active');
-  player0BoardEl.classList.add('active');
-  // remove winner class to player
-  player0BoardEl.classList.remove('player--winner');
-  player1BoardEl.classList.remove('player--winner');
-  // enable roll and hold buttons
-  rollBtn.disabled = false;
-  holdBtn.disabled = false;
+
+  for (let i = 0; i < 2; i++) {
+    scoresEl[i].textContent = '0';
+    playersBoardEl[i].classList.remove('player--active');
+    playersBoardEl[i].classList.remove('player--winner');
+  }
+
+  playersBoardEl[currentPlayer].classList.add('player--active');
+
+  enableButtons();
 }
 
-function updateUI() {}
-
 function checkWinner() {
-  if (player[currentPlayer] >= 10) {
-    // update player
-    currentPlayer === 0
-      ? player0BoardEl.classList.add('player--winner')
-      : player1BoardEl.classList.add('player--winner');
-    // disable roll and hold buttons
-    rollBtn.disabled = true;
-    holdBtn.disabled = true;
+  if (player[currentPlayer] >= 100) {
+    playersBoardEl[currentPlayer].classList.add('player--winner');
+
+    disableButtons();
   }
+}
+
+function disableButtons() {
+  rollBtn.disabled = true;
+  holdBtn.disabled = true;
+}
+
+function enableButtons() {
+  rollBtn.disabled = false;
+  holdBtn.disabled = false;
 }
