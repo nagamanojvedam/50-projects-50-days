805ef0e69410bb93b153dbb4238799753321170f
Vedam Naga Manoj <vedamnagamanoj@gmail.com>
2024-11-05 21:10:50 +0530

    added random choice picker

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -4,16 +4,16 @@ const textArea = document.querySelector('#textarea');
 const tagsContainer = document.querySelector('.tags');
 
 textArea.addEventListener('keyup', (evnt) => {
-  console.log(evnt.key);
   if (evnt.key === 'Enter') {
     pickChoice();
-  }
-  const input = textArea.value
-    .split(',')
-    .filter((item) => item.trim() !== '')
-    .map((item) => item.trim());
+  } else {
+    const input = textArea.value
+      .split(',')
+      .filter((item) => item.trim() !== '')
+      .map((item) => item.trim());
 
-  handleInput(input);
+    handleInput(input);
+  }
 });
 
 function handleInput(input) {
@@ -28,6 +28,42 @@ function handleInput(input) {
 }
 
 function pickChoice() {
-  console.log(`clicked Enter`);
-  textArea.textContent = '';
+  const times = 30;
+
+  textArea.value = '';
+
+  const interval = setInterval(() => {
+    const randomTag = getRandomTag();
+    addHighlight(randomTag);
+    setTimeout(() => {
+      removeHighlight(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    setTimeout(() => {
+      const resultTag = getRandomTag();
+      console.log(resultTag);
+      addHighlight(resultTag);
+    }, 100);
+  }, times * 100);
+
+  setTimeout(() => {
+    tagsContainer.innerHTML = '';
+  }, times * 2 * 100);
+}
+
+function getRandomTag() {
+  const tags = document.querySelectorAll('.tag');
+
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function addHighlight(tag) {
+  tag.classList.add('highlighted');
+}
+
+function removeHighlight(tag) {
+  tag.classList.remove('highlighted');
 }
