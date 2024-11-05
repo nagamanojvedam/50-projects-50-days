ae44672f87ba9bf2ad659a18daa4f4e36b1b85e2
Naga Manoj Vedam <nagamanojvedam@gmail.com>
2024-11-05 20:29:14 +0530

    in middle of random choice picker

--- random-choice-picker/script.js
+++ random-choice-picker/script.js
@@ -1,3 +1,33 @@
 'use strict';
 
-console.log('Random Choice Picker');
+const textArea = document.querySelector('#textarea');
+const tagsContainer = document.querySelector('.tags');
+
+textArea.addEventListener('keyup', (evnt) => {
+  console.log(evnt.key);
+  if (evnt.key === 'Enter') {
+    pickChoice();
+  }
+  const input = textArea.value
+    .split(',')
+    .filter((item) => item.trim() !== '')
+    .map((item) => item.trim());
+
+  handleInput(input);
+});
+
+function handleInput(input) {
+  tagsContainer.innerHTML = '';
+
+  input.forEach((item) => {
+    const newEl = document.createElement('span');
+    newEl.classList.add('tag');
+    newEl.textContent = item;
+    tagsContainer.appendChild(newEl);
+  });
+}
+
+function pickChoice() {
+  console.log(`clicked Enter`);
+  textArea.textContent = '';
+}
