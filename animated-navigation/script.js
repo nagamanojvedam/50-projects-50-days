9677dfe3d18436600f14abc1a2744441a3fae9c0
Vedam Naga Manoj <vedamnagamanoj@gmail.com>
2024-11-06 21:12:51 +0530

    incomplete animated navigation

--- animated-navigation/script.js
+++ animated-navigation/script.js
@@ -1,2 +1,9 @@
 'use strict';
 console.log('Animated Navigation');
+
+const nav = document.getElementById('nav');
+const toggle = document.getElementById('toggle');
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
